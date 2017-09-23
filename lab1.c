949b0171b20e4873283cb2404967037186e2beec
Quanley Marieatte <qmarieatte@hotmail.com>
2017-09-23 15:51:57 -0400

    Added configuration bits.

--- lab1.c
+++ lab1.c
@@ -3,6 +3,11 @@
 #include <p18f452.h>
 #include <stdio.h>
 
+/*Set configuration bits for use with PICKit3*/
+#pragma config OSC = HS
+#pragma config WDT = OFF
+#pragma config LVP = OFF
+
 int i = 0;
 
 void main (void){
